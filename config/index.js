51f3eb7eef008d83cef02917e289543e27740035
public16to <zj@zjdeMacBook-Pro.local>
2020-03-25 22:50:54 +0800

    fix bug

--- config/index.js
+++ config/index.js
@@ -66,6 +66,7 @@ module.exports = {
     // npm install --save-dev compression-webpack-plugin
     productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
+    productionSourceMap:false,
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
