447da0f563df6ae0a9d5bb5eb394a750cfca88f5
public16to <zj@zjdeMacBook-Pro.local>
2020-04-16 21:06:16 +0800

    fix bug

--- static/js/highlightCode.js
+++ static/js/highlightCode.js
@@ -3,10 +3,12 @@ import * as javascript from 'highlight.js/lib/languages/javascript'
 import * as python from 'highlight.js/lib/languages/python'
 import * as bash from 'highlight.js/lib/languages/bash'
 import * as html from 'highlight.js/lib/languages/htmlbars'
+import * as html from 'highlight.js/lib/languages/json'
 hljs.registerLanguage('javascript', javascript)
 hljs.registerLanguage('python', python)
 hljs.registerLanguage('bash', bash)
 hljs.registerLanguage('html', html)
+hljs.registerLanguage('json', json)
 import "highlight.js/styles/github.css";
 import "github-markdown-css";
 const highlightCode = () => {
